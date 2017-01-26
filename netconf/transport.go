ed2d6c400d0e867a2d08f053040078712d426b6f
Mislav Novakovic <mislav.novakovic@sartura.hr>
2017-01-26 18:33:35 +0100

    correct invalid namespace in hello message
    Signed-off-by: Mislav Novakovic <mislav.novakovic@sartura.hr>

--- netconf/transport.go
+++ netconf/transport.go
@@ -15,12 +15,12 @@ const (
 
 // DefaultCapabilities sets the default capabilities of the client library
 var DefaultCapabilities = []string{
-	"urn:ietf:params:xml:ns:netconf:base:1.0",
+	"urn:ietf:params:netconf:base:1.0",
 }
 
 // HelloMessage is used when bringing up a NetConf session
 type HelloMessage struct {
-	XMLName      xml.Name `xml:"hello"`
+	XMLName      xml.Name `xml:"urn:ietf:params:xml:ns:netconf:base:1.0 hello"`
 	Capabilities []string `xml:"capabilities>capability"`
 	SessionID    int      `xml:"session-id,omitempty"`
 }
