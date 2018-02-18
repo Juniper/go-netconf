d59df5c8569df36ecc55e593dece66155079c816
Jade Auer <jda@tapodi.net>
2018-02-18 11:41:45 -0800

    write some tests for rpc

--- netconf/rpc_test.go
+++ netconf/rpc_test.go
@@ -4,6 +4,46 @@ import (
 	"testing"
 )
 
+func TestRPCErrorError(t *testing.T) {
+	rpcErr := RPCError{
+		Severity: "lol",
+		Message:  "cats",
+	}
+	expected := "netconf rpc [lol] 'cats'"
+
+	errMsg := rpcErr.Error()
+	if errMsg != expected {
+		t.Errorf("expected: %s, got: %s", expected, errMsg)
+	}
+}
+
+func TestMethodLock(t *testing.T) {
+	expected := "<lock><target><what.target/></target></lock>"
+
+	mLock := MethodLock("what.target")
+	if mLock.MarshalMethod() != expected {
+		t.Errorf("got %s, expected %s", mLock, expected)
+	}
+}
+
+func TestMethodUnlock(t *testing.T) {
+	expected := "<unlock><target><what.target/></target></unlock>"
+
+	mUnlock := MethodUnlock("what.target")
+	if mUnlock.MarshalMethod() != expected {
+		t.Errorf("got %s, expected %s", mUnlock, expected)
+	}
+}
+
+func TestMethodGetConfig(t *testing.T) {
+	expected := "<get-config><source><what.target/></source></get-config>"
+
+	mGetConfig := MethodGetConfig("what.target")
+	if mGetConfig.MarshalMethod() != expected {
+		t.Errorf("got %s, expected %s", mGetConfig, expected)
+	}
+}
+
 // TestUUIDLength verifies that UUID length is cor([a-zA-Z]|\d|-)rect
 func TestUUIDLength(t *testing.T) {
 	expectedLength := 36
