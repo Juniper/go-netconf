afa1326a6cdf5b1ea560257b69e3fd965388b2da
David Gee <david.john.gee@gmail.com>
2018-06-23 20:52:33 +0100

    Minor tweak to client. Now has -version flag

--- client.go
+++ client.go
@@ -3,25 +3,37 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"log"
+	"os"
 
 	"./netconf"
 
 	"golang.org/x/crypto/ssh"
 )
 
+const versionstring string = "0.2"
+
 func main() {
 
+	var version = flag.Bool("version", false, "Show version for code")
 	var targethost = flag.String("targethost", "", "Target IPv4 or FQDN hostname of a NETCONF node")
 	/*var transport = */ flag.String("transport", "ssh", "Transport mode (NOT IMPLEMENTED)")
 	var envelope = flag.String("envelope", "", "XML Envelope")
+	var envelopefile = flag.String("envelopefile", "", "XML envelope file path")
 	var username = flag.String("username", "", "Username")
 	var password = flag.String("password", "", "Password")
 	var port = flag.Int("port", 830, "Port for accessing NETCONF")
 	/*var sshkey = */ flag.String("sshkey", "", "SSHKey for accessing node (NOT IMPLEMENTED)")
 	flag.Parse()
 
-	// TODO
+	// If version has been requested
+	if *version {
+		fmt.Printf("Version is: %s", versionstring)
+		os.Exit(0)
+	}
+
+	// TODO(davidjohngee)
 	// Make some decisions over auth mechanism.
 	// 1. If username & password is present, ignore SSH key
 	// 2. If SSH key is present, ignore username and password
@@ -41,7 +53,22 @@ func main() {
 
 	defer s.Close()
 
-	reply, err := s.Exec(netconf.RawMethod(*envelope))
+	// netconfcall content var
+	netconfcall := ""
+	// If the flag 'envelopefile' is set, then let's read the content into memory instead of using envelope
+	if *envelopefile != "" {
+		// Read content from disk
+		bytes, err := ioutil.ReadFile(*envelopefile)
+		netconfcall = string(bytes)
+		if err != nil {
+			log.Panic(err)
+		}
+		// Else, consume content of envelope
+	} else {
+		netconfcall = *envelope
+	}
+
+	reply, err := s.Exec(netconf.RawMethod(netconfcall))
 	if err != nil {
 		panic(err)
 	}
