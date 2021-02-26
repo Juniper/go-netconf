bb51e84ff02ba88cf9a968888a652402ddc96249
Tobbeman <Tobbeman1994@gmail.com>
2021-02-26 17:53:50 +0100

    Add return error on write actions (#104)
    This error can be used to detect if the connection was broken,
    for example if the server goes down between client sending data
    
    Co-authored-by: Tobias Laving <lavingtobias@gmail.com>

--- netconf/transport.go
+++ netconf/transport.go
@@ -74,9 +74,9 @@ func (t *transportBasicIO) Send(data []byte) error {
 	}
 	dataInfo = append(dataInfo, data...)
 	dataInfo = append(dataInfo, seperator...)
-	t.Write(dataInfo)
+	_, err := t.Write(dataInfo)
 
-	return nil // TODO: Implement error handling!
+	return err
 }
 
 func (t *transportBasicIO) Receive() ([]byte, error) {
