ba55ce38751c8eb5fd2f83e960911bd70426d632
Brandon Bennett <brandon@brbe.me>
2023-04-10 14:59:22 -0600

    unittests for HelloMsg marshalling/unmarshalling (#37)
    Some basic testing for `HelloMsg` xml encoding/decoding. I originally
    held off on testing these directly but I don't think it's a bad idea.
    
    **Test Plan**
    
    ```
    $ go test . -v -run=HelloMsg
    === RUN   TestUnmarshalHelloMsg
    === RUN   TestUnmarshalHelloMsg/basic
    === RUN   TestUnmarshalHelloMsg/junos
    --- PASS: TestUnmarshalHelloMsg (0.00s)
        --- PASS: TestUnmarshalHelloMsg/basic (0.00s)
        --- PASS: TestUnmarshalHelloMsg/junos (0.00s)
    === RUN   TestMarshalHelloMsg
    === RUN   TestMarshalHelloMsg/basic
        msg_test.go:88: out: <hello xmlns="urn:ietf:params:xml:ns:netconf:base:1.0"><capabilities><capability>urn:ietf:params:netconf:base:1.0</capability><capability>urn:ietf:params:netconf:base:1.1</capability></capabilities></hello>
    === RUN   TestMarshalHelloMsg/junos
        msg_test.go:88: out: <hello xmlns="urn:ietf:params:xml:ns:netconf:base:1.0"><session-id>410</session-id><capabilities><capability>urn:ietf:params:netconf:base:1.0</capability><capability>urn:ietf:params:netconf:capability:candidate:1.0</capability><capability>urn:ietf:params:netconf:capability:confirmed-commit:1.0</capability><capability>urn:ietf:params:netconf:capability:validate:1.0</capability><capability>urn:ietf:params:netconf:capability:url:1.0?scheme=http,ftp,file</capability><capability>urn:ietf:params:xml:ns:netconf:base:1.0</capability><capability>urn:ietf:params:xml:ns:netconf:capability:candidate:1.0</capability><capability>urn:ietf:params:xml:ns:netconf:capability:confirmed-commit:1.0</capability><capability>urn:ietf:params:xml:ns:netconf:capability:validate:1.0</capability><capability>urn:ietf:params:xml:ns:netconf:capability:url:1.0?scheme=http,ftp,file</capability><capability>urn:ietf:params:xml:ns:yang:ietf-netconf-monitoring</capability><capability>http://xml.juniper.net/netconf/jdm/1.0</capability></capabilities></hello>
    --- PASS: TestMarshalHelloMsg (0.00s)
        --- PASS: TestMarshalHelloMsg/basic (0.00s)
        --- PASS: TestMarshalHelloMsg/junos (0.00s)
    PASS
    ok      github.com/nemith/netconf       0.382s
    ```

--- msg_test.go
+++ msg_test.go
@@ -7,6 +7,90 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+var ()
+
+var helloMsgTestTable = []struct {
+	name string
+	raw  []byte
+	msg  HelloMsg
+}{
+	{
+		name: "basic",
+		raw:  []byte(`<hello xmlns="urn:ietf:params:xml:ns:netconf:base:1.0"><capabilities><capability>urn:ietf:params:netconf:base:1.0</capability><capability>urn:ietf:params:netconf:base:1.1</capability></capabilities></hello>`),
+		msg: HelloMsg{
+			XMLName: xml.Name{
+				Local: "hello",
+				Space: "urn:ietf:params:xml:ns:netconf:base:1.0",
+			},
+			Capabilities: []string{
+				"urn:ietf:params:netconf:base:1.0",
+				"urn:ietf:params:netconf:base:1.1",
+			},
+		},
+	},
+	{
+		name: "junos",
+		raw: []byte(`<hello xmlns="urn:ietf:params:xml:ns:netconf:base:1.0">
+  <capabilities>
+      <capability>urn:ietf:params:netconf:base:1.0</capability>
+	  <capability>urn:ietf:params:netconf:capability:candidate:1.0</capability>
+	  <capability>urn:ietf:params:netconf:capability:confirmed-commit:1.0</capability>
+	  <capability>urn:ietf:params:netconf:capability:validate:1.0</capability>
+	  <capability>urn:ietf:params:netconf:capability:url:1.0?scheme=http,ftp,file</capability>
+	  <capability>urn:ietf:params:xml:ns:netconf:base:1.0</capability>
+	  <capability>urn:ietf:params:xml:ns:netconf:capability:candidate:1.0</capability>
+	  <capability>urn:ietf:params:xml:ns:netconf:capability:confirmed-commit:1.0</capability>
+	  <capability>urn:ietf:params:xml:ns:netconf:capability:validate:1.0</capability>
+	  <capability>urn:ietf:params:xml:ns:netconf:capability:url:1.0?scheme=http,ftp,file</capability>
+	  <capability>urn:ietf:params:xml:ns:yang:ietf-netconf-monitoring</capability>
+	  <capability>http://xml.juniper.net/netconf/jdm/1.0</capability>
+  </capabilities>
+  <session-id>410</session-id>
+</hello>`),
+		msg: HelloMsg{
+			XMLName: xml.Name{
+				Local: "hello",
+				Space: "urn:ietf:params:xml:ns:netconf:base:1.0",
+			},
+			Capabilities: []string{
+				"urn:ietf:params:netconf:base:1.0",
+				"urn:ietf:params:netconf:capability:candidate:1.0",
+				"urn:ietf:params:netconf:capability:confirmed-commit:1.0",
+				"urn:ietf:params:netconf:capability:validate:1.0",
+				"urn:ietf:params:netconf:capability:url:1.0?scheme=http,ftp,file",
+				"urn:ietf:params:xml:ns:netconf:base:1.0",
+				"urn:ietf:params:xml:ns:netconf:capability:candidate:1.0",
+				"urn:ietf:params:xml:ns:netconf:capability:confirmed-commit:1.0",
+				"urn:ietf:params:xml:ns:netconf:capability:validate:1.0",
+				"urn:ietf:params:xml:ns:netconf:capability:url:1.0?scheme=http,ftp,file",
+				"urn:ietf:params:xml:ns:yang:ietf-netconf-monitoring",
+				"http://xml.juniper.net/netconf/jdm/1.0",
+			},
+			SessionID: 410,
+		},
+	},
+}
+
+func TestUnmarshalHelloMsg(t *testing.T) {
+	for _, tc := range helloMsgTestTable {
+		t.Run(tc.name, func(t *testing.T) {
+			var got HelloMsg
+			err := xml.Unmarshal(tc.raw, &got)
+			assert.NoError(t, err)
+			assert.Equal(t, got, tc.msg)
+		})
+	}
+}
+func TestMarshalHelloMsg(t *testing.T) {
+	for _, tc := range helloMsgTestTable {
+		t.Run(tc.name, func(t *testing.T) {
+			out, err := xml.Marshal(tc.msg)
+			t.Logf("out: %s", out)
+			assert.NoError(t, err)
+		})
+	}
+}
+
 func TestMarshalRPCMsg(t *testing.T) {
 	tt := []struct {
 		name      string
